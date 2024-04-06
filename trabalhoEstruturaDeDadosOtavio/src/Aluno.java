889b51e41916f08e1ee6c1fb0974d51e35c9f2ec
Otavio Marques <marquesotavio251204@gmail.com>
2024-04-06 20:40:48 -0300

    finalizando trabalho

--- trabalhoEstruturaDeDadosOtavio/src/Aluno.java
+++ trabalhoEstruturaDeDadosOtavio/src/Aluno.java
@@ -47,4 +47,19 @@ public class Aluno {
                 '}';
     }
 
+    // Exercício 4 - Comparação de objetos:
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(matricula, aluno.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matricula);
+    }
 }
--- trabalhoEstruturaDeDadosOtavio/src/TrabalhoJava.java
+++ trabalhoEstruturaDeDadosOtavio/src/TrabalhoJava.java
@@ -1,6 +1,5 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TrabalhoJava {
     public static void main(String[] args) {
@@ -11,23 +10,103 @@ public class TrabalhoJava {
         Aluno aluno2 = new Aluno("Maria", LocalDate.of(2001, 8, 23), 1002);
         Aluno aluno3 = new Aluno("Pedro", LocalDate.of(1999, 12, 10), 1003);
 
-        List<Aluno> alunos = new ArrayList<>();
+        List<Aluno> alunosList = new ArrayList<>();
 
-        alunos.add(aluno1);
-        alunos.add(aluno2);
-        alunos.add(aluno3);
+        alunosList.add(aluno1);
+        alunosList.add(aluno2);
+        alunosList.add(aluno3);
 
-        for(Aluno aluno : alunos) {
+        for(Aluno aluno : alunosList) {
             System.out.println(aluno);
         }
 
         // Exercício 3 - Listas dinâmicas:
         System.out.println("Exercício 3 - Listas dinâmicas");
-        alunos.remove(0);
-        alunos.remove(1);
+        alunosList.remove(0);
+        alunosList.remove(1);
 
-        for(Aluno aluno : alunos) {
+        for(Aluno aluno : alunosList) {
             System.out.println(aluno);
         }
+
+       // Exercício 5 - Conjuntos (Set):
+        System.out.println("Exercício 5 - Conjuntos (Set)");
+        Set<Aluno> alunosSet = new HashSet<>();
+
+        Aluno aluno4 = new Aluno("Otavio", LocalDate.of(1999, 12, 10), 1003);
+
+        alunosSet.add(aluno1);
+        alunosSet.add(aluno2);
+        alunosSet.add(aluno3);
+        alunosSet.add(aluno4);
+
+        for(Aluno aluno : alunosSet){
+            System.out.println(aluno);
+        }
+
+        //Exercício 6 - Conjuntos (Set):
+        System.out.println("Exercício 6 - Conjuntos (Set)");
+
+        Set<Aluno> alunoSetUnion1 = new HashSet<>();
+        Set<Aluno> alunoSetUnion2 = new HashSet<>();
+
+        alunoSetUnion1.add(aluno1);
+        alunoSetUnion1.add(aluno2);
+
+        alunoSetUnion2.add(aluno2);
+        alunoSetUnion2.add(aluno3);
+
+        alunoSetUnion1.addAll(alunoSetUnion2);
+
+        for(Aluno aluno : alunoSetUnion1){
+            System.out.println(aluno);
+        }
+
+        //Exercício 7 - Filas (LinkedList):
+        System.out.println("Exercício 7 - Filas (LinkedList)");
+
+        LinkedList<Aluno> alunoQueueEx7 = new LinkedList<>();
+
+        alunoQueueEx7.offer(aluno1);
+        alunoQueueEx7.offer(aluno2);
+
+        Aluno alunoRemoveQueueEx7 = alunoQueueEx7.removeFirst();
+
+        System.out.println(alunoRemoveQueueEx7.getNome());
+
+        // Exercício 8 - Filas (LinkedList):
+        System.out.println("Exercício 8 - Filas (LinkedList):");
+
+        LinkedList<Aluno> alunoQueueEx8 = new LinkedList<>();
+
+        alunoQueueEx8.offer(aluno1);
+        alunoQueueEx8.offer(aluno2);
+
+        Aluno alunoRemoveQueueEx8 = alunoQueueEx8.removeLast();
+        System.out.println(alunoRemoveQueueEx8.getNome());
+
+        // Exercício 9 - Pilhas (Stack)
+        System.out.println("Exercício 9 - Pilhas (Stack)");
+
+        Stack<Aluno> alunoStackEx9 = new Stack<>();
+
+        alunoStackEx9.push(aluno1);
+        alunoStackEx9.push(aluno2);
+
+        Aluno alunoRemoveStackEx9 = alunoStackEx9.remove(0);
+
+        System.out.println(alunoRemoveStackEx9.getNome());
+
+        // Exercício 10 - Pilhas (Stack)
+        System.out.println("Exercício 10 - Pilhas (Stack)");
+
+        Stack<Aluno> alunoStackEx10 = new Stack<>();
+
+        alunoStackEx10.push(aluno1);
+        alunoStackEx10.push(aluno2);
+
+        Aluno alunoStackRemoveEx10 = alunoStackEx10.pop();
+
+        System.out.println(alunoStackRemoveEx10.getNome());
     }
 }
